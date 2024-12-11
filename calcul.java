9aa8fbcd94c64fd17afe07cac4f467a4b6d3654a
ikerambenazizi <ikerambenazizi595@gmail.com>
2024-12-11 10:00:22 +0100

    creation du menu

--- calcul.java
+++ calcul.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class Main {
+	 static  void menu(){
+
+        System.out.println("1-addition");
+        System.out.println("2-soustraction");
+        System.out.println("3-multiplication");
+        System.out.println("4-division");
+
+    }
 	public static void main(String[] args){
         double x,y,rs;
          int choix;
 
 
-
+		 menu();
 
 	    }
 }
