745e8fa1efddad891683aba70affae3901e2d3a0
ikerambenazizi <ikerambenazizi595@gmail.com>
2024-12-11 10:07:42 +0100

    finission du fonction addition

--- calcul.java
+++ calcul.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 public class Main {
+	static double addtion(double a , double b){
+        return  a+b;
+    }
 	 static  void menu(){
 
         System.out.println("1-addition");
-        System.out.println("2-soustraction");
-        System.out.println("3-multiplication");
-        System.out.println("4-division");
 
     }
 	public static void main(String[] args){
         double x,y,rs;
          int choix;
 
+		do{
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Enter num 1 : ");
+         x = scanner.nextDouble();
+         System.out.println("Enter num 2 : ");
+         y = scanner.nextDouble();
 
 		 menu();
+		 System.out.println("Enter voter choix : ");
+         choix = scanner.nextInt();
+         switch (choix){
+             case 1 :
+                rs = addtion(x,y);
+                System.out.println("le resultat est :"+rs);
+        break;
+	default:
+                 System.out.println("Choix invalide ");
+		 }
+        }while (choix !=2);
 
 	    }
 }
